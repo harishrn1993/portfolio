1f8fbabc5c2ecd4ade15699c0e3b1fc74a36054b
harishrn1993 <harish.rn1993@gmail.com>
2023-01-20 00:00:24 +0530

    commenting out test files

--- src/app/experience/experience.component.spec.ts
+++ src/app/experience/experience.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ExperienceComponent } from './experience.component';
+// import { ExperienceComponent } from './experience.component';
 
-describe('ExperienceComponent', () => {
-  let component: ExperienceComponent;
-  let fixture: ComponentFixture<ExperienceComponent>;
+// describe('ExperienceComponent', () => {
+//   let component: ExperienceComponent;
+//   let fixture: ComponentFixture<ExperienceComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ExperienceComponent]
-    })
-      .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ExperienceComponent]
+//     })
+//       .compileComponents();
 
-    fixture = TestBed.createComponent(ExperienceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(ExperienceComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
