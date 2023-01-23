c5247dd36bf9a2fb117a6185d380012099b8ade9
harishrn1993 <harish.rn1993@gmail.com>
2023-01-24 00:57:08 +0530

    adding css responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  @ViewChild('navBarList') navBarList: ElementRef | undefined;
+
+  toggleButtonClicked() {
+    console.log(this.navBarList?.nativeElement.classList)
+    this.navBarList?.nativeElement.classList.toggle('active')
+  }
+
+  onNavListSelected() {
+    this.navBarList?.nativeElement.classList.toggle('active')
+  }
+
+
 }
